464cb4b3ac6ae93dc027f7f8c498be61eb06ddf3
Alexander Fuchs <alexander.fuchs@kurz.de>
2021-09-16 13:28:43 +0200

    Version 1.0.8 (overrideChildTca for tx_news)

---
+++ Configuration/TCA/Overrides/tx_news_domain_model_news.php
@@ -0,0 +1,20 @@
+<?php
+defined('TYPO3_MODE') or die();
+
+
+
+    $GLOBALS['TCA']['tx_news_domain_model_news']['columns']['fal_media']['config']['overrideChildTca']['types'][4] = [
+        'showitem' => '
+        --palette--;LLL:EXT:lang/Resources/Private/Language/locallang_tca.xlf:sys_file_reference.imageoverlayPalette;newsPalette,
+        --palette--;;videoOverlayPalette,
+        --palette--;;filePalette'
+    ];
+
+$GLOBALS['TCA']['tx_news_domain_model_news']['columns']['fal_media']['config']['overrideChildTca']['types'][0] = [
+    'showitem' => '
+        --palette--;LLL:EXT:lang/Resources/Private/Language/locallang_tca.xlf:sys_file_reference.imageoverlayPalette;newsPalette,
+        --palette--;;videoOverlayPalette,
+        --palette--;;filePalette'
+];
+
+
